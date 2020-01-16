13ce9cf4f6c1296eca468f36447bfe337ed17c61
Ana0833 <58481417+Ana0833@users.noreply.github.com>
2020-01-16 19:31:51 +0100

    Add files via upload

---
+++ thesis copy/js/tips.js
@@ -0,0 +1,235 @@
+$(document).ready(function(){
+	Tipped.create('#tooltip1', {
+		inline: 'tooltip1-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip2', {
+		inline: 'tooltip2-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip3', {
+		inline: 'tooltip3-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip4', {
+		inline: 'tooltip4-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip5', {
+		inline: 'tooltip5-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip6', {
+		inline: 'tooltip6-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip7', {
+		inline: 'tooltip7-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip8', {
+		inline: 'tooltip8-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip9', {
+		inline: 'tooltip9-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip10', {
+		inline: 'tooltip10-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip11', {
+		inline: 'tooltip11-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip12', {
+		inline: 'tooltip12-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip13', {
+		inline: 'tooltip13-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip14', {
+		inline: 'tooltip14-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip15', {
+		inline: 'tooltip15-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip16', {
+		inline: 'tooltip16-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip17', {
+		inline: 'tooltip17-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip18', {
+		inline: 'tooltip18-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip19', {
+		inline: 'tooltip19-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip20', {
+		inline: 'tooltip20-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+	Tipped.create('#tooltip21', {
+		inline: 'tooltip21-content',
+		skin: 'dark',
+		radius: false,
+		padding: true,
+		position: 'topleft',
+		size: 'large',
+		close: true,
+		hideOn: false,
+		shadow: false
+	});
+
+	
+});
